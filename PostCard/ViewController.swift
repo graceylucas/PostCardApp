01be71bd51451cbc686be60a0aff0ec511cf73fe
Mary Grace Lucas <marygracelucas1@Marys-MacBook-Pro.local>
2015-05-31 11:58:56 -0400

    Initial Commit

---
+++ PostCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PostCard
+//
+//  Created by Mary Grace Lucas on 5/31/15.
+//  Copyright (c) 2015 Mary Grace Lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
