24e322a071d934a6e54447fba4dd8e8618a45530
Mary Grace Lucas <marygracelucas1@Marys-MacBook-Pro.local>
2015-06-21 13:57:53 -0400

    first rev of project

--- PostCard/ViewController.swift
+++ PostCard/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func sendMessageButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        enterMessageTextField.text = ""
+        resignEveryKeyboard()
+        messageLabel.textColor = UIColor.redColor()
+        
+        mailButton.setTitle("MAIL SENT", forState: UIControlState.Normal)
+        
+        
+    }
+    
+    //Added this function as a way to practice trimmer coding
+    func resignEveryKeyboard () {
+        enterMessageTextField.resignFirstResponder()
+        enterNameTextField.resignFirstResponder()
+    }
+    
+    
 }
-
